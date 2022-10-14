1f4f9813127426d3c071e47251b95e3240933993
yusufyay <89834145+yusufyay@users.noreply.github.com>
2022-10-14 14:53:34 +0300

    14 Ekim Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,55 @@
-console.log("Merhaba Kodlama.io")
 
-//JS type safe değildir
-let dolarBugun = 9.30
-
-let dolarDun = 9.20
-dolarDun = "9.20"
-{
-    let dolarDun = 9.10
-}
-
-console.log(dolarDun)
-
-const euroDun = 11.2
-//euroDun = 11
-
-console.log(euroDun)
-
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+let users = [5,"Emlak konut kredisi",["dami kredi",3,"a"]]
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+for (let i = 0; i < users.length; i++) {
+    console.log("<li>"+users[i]+"</li>")
+    
 }
+
 console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
 
-console.log(konutKredileri)
+
+
+
+
+
+
+// console.log("Merhaba Kodlama.io")
+
+// //JS type safe değildir
+// let dolarBugun = 9.30
+
+// let dolarDun = 9.20
+// dolarDun = "9.20"
+// {
+//     let dolarDun = 9.10
+// }
+
+// console.log(dolarDun)
+
+// const euroDun = 11.2
+// //euroDun = 11
+
+// console.log(euroDun)
+
+// //array
+// //camelCasing
+// //PascalCasing
+// let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+// console.log("<ul>")
+// for(let i = 0;i<konutKredileri.length;i++){
+//     console.log("<li>"+konutKredileri[i]+"</li>")
+// }
+// console.log("</ul>")
+
+// {/* <ul>
+//     <li>Konut kredisi</li>
+//     <li>Emlak Konut Kredisi</li>
+//     <li>Kamu Konut Kredisi</li>
+// </ul> */}
+
+// console.log(konutKredileri)
+
